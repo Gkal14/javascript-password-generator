2b8fbd5c2a0bc558b1c5bca8dfb7bf789c2415ff
George Kaluwin <georgekaluwin@gmail.com>
2022-03-28 14:32:12 +1030

    password parameters set

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+var pwdCriteria = {
+
+    pwdlength: 128,
+
+    pwdLowerCase: ["abcdefghijklmnopqrstuvwxyz"],
+
+    pwdUpperCase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+
+    pwdNumeric: [0,1,2,3,4,5,6,7,8,9],
+
+    pwdCharacter: [ "!","#","$","%","&","'","(",")","*","+","-",".","/",
+    ":",";","<","=",">","?","@","[","\"","]",",","^","_","`","{","|","}","~"],
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
