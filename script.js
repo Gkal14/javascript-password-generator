462d34d3bbe888e76c084d52620e04c2a5da5e00
George Kaluwin <georgekaluwin@gmail.com>
2022-03-28 15:50:16 +1030

    corrected IF statement to generate prompt

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var generateBtn = document.querySelector("#generate");
 
 var pwdCriteria = {
 
-    pwdlength: 128,
+    pwdlength: 0,
 
-    pwdLowerCase: ["abcdefghijklmnopqrstuvwxyz"],
+    pwdLowerCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
 
-    pwdUpperCase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+    pwdUpperCase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
 
     pwdNumeric: [0,1,2,3,4,5,6,7,8,9],
 
@@ -31,19 +31,38 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
  var result=""; 
 
- var pwdlength= 128;
+ var pwdlength= 0;
  var pwdLowerCase;
  var pwdUpperCase;
  var pwdNumeric;
  var pwdCharacter;
 
- pwdlength= 128;
- pwdCriteria.pwdlength=128;
+ pwdlength= 0;
+ pwdCriteria.pwdlength=0;
  result="";
 
+// checking for password criteria
+if (pwdlength <8 || pwdlength>128) {
+  pwdlength=prompt("Your password must be between 8 and 128 characters long. How long do you want your secure password to be?");
 
-if (pwdlength >8 && pwdlength<128) {
-  pwdlength=prompt("Your password must be between 8 and 128 characters long. How long do you want your secure password to be?")
+if (pwdlength===null){
+  return "Password Generator";
+} 
+else {
+  if (!isFinite(pwdlength)) {
+    alert ("You must enter a number");
+    return "Password Generator";
+  }
+  else {
+    if (pwdlength <8 && pwdlength>128) {
+      alert ("Your password must be between 8 and 128 characters long");
+      return "Password Generator"
+  }
+  else {
+   
+  }
+
 }
 }
-
+}
+}
